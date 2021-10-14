ba07061ad7e0712f1d41a394bd93a6d0cd037ee8
hermes0578 <91257081+hermes0578@users.noreply.github.com>
2021-10-15 08:39:55 +0900

    Update params.cc

--- selfdrive/common/params.cc
+++ selfdrive/common/params.cc
@@ -216,6 +216,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"LongControlEnabled", PERSISTENT},
     {"MadModeEnabled", PERSISTENT},
 
+    {"SmartMDPS", PERSISTENT},
     {"IsLdwsCar", PERSISTENT},
     {"LaneChangeEnabled", PERSISTENT},
     {"AutoLaneChangeEnabled", PERSISTENT},
