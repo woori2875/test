d631c809063e0daaf815cb662a93fef2a2b27e7d
hermes0578 <91257081+hermes0578@users.noreply.github.com>
2021-10-16 23:05:55 +0900

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -47,7 +47,7 @@ class CarInterface(CarInterfaceBase):
     ret.maxSteeringAngleDeg = 1000.
 
     
-    # ---PID
+    # -------------PID
     if Params().get("LateralControlSelect", encoding='utf8') == "0":
       if candidate in [CAR.GENESIS, CAR.GENESIS_G80, CAR.GENESIS_EQ900]:
           ret.lateralTuning.pid.kf = 0.000038
@@ -63,18 +63,8 @@ class CarInterface(CarInterfaceBase):
           ret.steerRateCost = 0.5
           ret.steerLimitTimer = 2.0
           ret.steerRatio = 14.8
-      else:
-          ret.lateralTuning.pid.kf = 0.00005
-          ret.lateralTuning.pid.kpBP = [0.]
-          ret.lateralTuning.pid.kpV = [0.25]
-          ret.lateralTuning.pid.kiBP = [0.]
-          ret.lateralTuning.pid.kiV = [0.05]
-          
-          ret.steerActuatorDelay = 0.2
-          ret.steerRateCost = 0.6
-          ret.steerLimitTimer = 2.0
-          ret.steerRatio = 16.2
-    # ---INDI
+    
+    # ---------------INDI
     elif Params().get("LateralControlSelect", encoding='utf8') == "1":
       if candidate in [CAR.GENESIS, CAR.GENESIS_EQ900]:
           ret.lateralTuning.init('indi')
@@ -91,23 +81,8 @@ class CarInterface(CarInterfaceBase):
           ret.steerRateCost = 0.5
           ret.steerLimitTimer = 2.0
           ret.steerRatio = 16.5
-      else:
-          ret.lateralTuning.init('indi')
-          ret.lateralTuning.indi.innerLoopGainBP = [0.]
-          ret.lateralTuning.indi.innerLoopGainV = [3.5]
-          ret.lateralTuning.indi.outerLoopGainBP = [0.]
-          ret.lateralTuning.indi.outerLoopGainV = [2.0]
-          ret.lateralTuning.indi.timeConstantBP = [0.]
-          ret.lateralTuning.indi.timeConstantV = [1.4]
-          ret.lateralTuning.indi.actuatorEffectivenessBP = [0.]
-          ret.lateralTuning.indi.actuatorEffectivenessV = [2.3]
-          
-          ret.steerRatio = 16.5
-          ret.steerActuatorDelay = 0.1
-          ret.steerLimitTimer = 2.5
-          ret.steerRateCost = 0.4
   
-    # -----------------LQR
+    # ---------------LQR
     elif Params().get("LateralControlSelect", encoding='utf8') == "2":
       if candidate in [CAR.GENESIS, CAR.GENESIS_G80, CAR.GENESIS_EQ900]:
           ret.lateralTuning.init('lqr')
@@ -124,25 +99,8 @@ class CarInterface(CarInterfaceBase):
           ret.steerActuatorDelay = 0.1
           ret.steerLimitTimer = 2.5
           ret.steerRateCost = 0.5
-          
-      else:
-          ret.lateralTuning.init('lqr')
-          ret.lateralTuning.lqr.scale = 1900.0
-          ret.lateralTuning.lqr.ki = 0.03
-          ret.lateralTuning.lqr.dcGain = 0.003
-          ret.lateralTuning.lqr.a = [0., 1., -0.22619643, 1.21822268]
-          ret.lateralTuning.lqr.b = [-1.92006585e-04, 3.95603032e-05]
-          ret.lateralTuning.lqr.c = [1., 0.]
-          ret.lateralTuning.lqr.k = [-105.0, 450.0]
-          ret.lateralTuning.lqr.l = [0.22, 0.318]
-          
-          ret.steerRatio = 16.5
-          ret.steerActuatorDelay = 0.1
-          ret.steerLimitTimer = 2.5
-          ret.steerRateCost = 0.4
-    # -------------------END
-    ret.steerMaxBP = [0.]
-    ret.steerMaxV = [1.5]
+          ret.steerMaxBP = [0.]
+          ret.steerMaxV = [1.5]
   
     # longitudinal
     ret.longitudinalTuning.kpBP = [0., 10.*CV.KPH_TO_MS, 20.*CV.KPH_TO_MS, 40.*CV.KPH_TO_MS, 70.*CV.KPH_TO_MS, 100.*CV.KPH_TO_MS, 130.*CV.KPH_TO_MS]
