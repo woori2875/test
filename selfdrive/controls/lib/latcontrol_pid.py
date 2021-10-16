4dcfe9957bfabe27ece8733a482787c51376cd5e
hermes0578 <91257081+hermes0578@users.noreply.github.com>
2021-10-16 20:40:00 +0900

    Update latcontrol_pid.py

--- selfdrive/controls/lib/latcontrol_pid.py
+++ selfdrive/controls/lib/latcontrol_pid.py
@@ -1,21 +1,23 @@
 import math
 
-from selfdrive.controls.lib.pid import PIDController
+from selfdrive.controls.lib.pid import LatPIDController
 from selfdrive.controls.lib.drive_helpers import get_steer_max
 from selfdrive.controls.lib.latcontrol import LatControl, MIN_STEER_SPEED
 from cereal import log
+from common.op_params import opParams
 
 
 class LatControlPID(LatControl):
   def __init__(self, CP, CI):
     super().__init__(CP)
-    self.pid = PIDController ((CP.lateralTuning.pid.kpBP, CP.lateralTuning.pid.kpV),
+    self.pid = LatPIDController ((CP.lateralTuning.pid.kpBP, CP.lateralTuning.pid.kpV),
                               (CP.lateralTuning.pid.kiBP, CP.lateralTuning.pid.kiV),
                               (CP.lateralTuning.pid.kdBP, CP.lateralTuning.pid.kdV),
                               k_f=CP.lateralTuning.pid.kf, pos_limit=1.0, neg_limit=-1.0,
                               derivative_period=0.1)
     self.new_kf_tuned = CP.lateralTuning.pid.newKfTuned
     self.get_steer_feedforward = CI.get_steer_feedforward
+    self.op_params = opParams()
 
   def reset(self):
     super().reset()
@@ -45,7 +47,7 @@ class LatControlPID(LatControl):
       deadzone = 0.0
 
       output_steer = self.pid.update(angle_steers_des, CS.steeringAngleDeg, override=CS.steeringPressed,
-                                     feedforward=steer_feedforward, speed=CS.vEgo, deadzone=deadzone)
+                                     feedforward=steer_feedforward * self.op_params.get('lat_f_multiplier'), speed=CS.vEgo, deadzone=deadzone)
       pid_log.active = True
       pid_log.p = self.pid.p
       pid_log.i = self.pid.i
