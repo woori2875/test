be95a10a0a2d573d31ba6e24d3995649872a9451
durango-op <71580184+durango-op@users.noreply.github.com>
2021-10-20 12:08:12 +0900

    Revert "Update gitpull.sh"
    This reverts commit 155d7fb448834d2eb88196cbb3d768d0ba391f8a.

--- gitpull.sh
+++ gitpull.sh
@@ -1,16 +1,9 @@
 #!/usr/bin/bash
 
 cd /data/openpilot
-
-if [ -f /EON ]; then
-  echo -n "0" > /data/params/d/PutPrebuilt
-  rm -f prebuilt
-elif [ -f /TICI ]; then
-  echo -n "0" > /data/params/d/PutPrebuilt
-  sudo rm -f prebuilt
-fi
-
+echo -n "0" > /data/params/d/PutPrebuilt
+rm -f prebuilt
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 git fetch --all
 REMOTE_HASH=$(git rev-parse --short --verify origin/$BRANCH)
-git reset --hard $REMOTE_HASH
+git reset --hard $REMOTE_HASH 
