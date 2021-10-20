9b918dd3e24c3b68e48e76c927d694f83456c25e
hermes0578 <91257081+hermes0578@users.noreply.github.com>
2021-10-21 08:46:49 +0900

    Update lateral_planner.py

--- selfdrive/controls/lib/lateral_planner.py
+++ selfdrive/controls/lib/lateral_planner.py
@@ -12,13 +12,13 @@ from selfdrive.config import Conversions as CV
 import cereal.messaging as messaging
 from cereal import log
 
-AUTO_LCA_START_TIME = 0.5
+AUTO_LCA_START_TIME = 0.0
 
 LaneChangeState = log.LateralPlan.LaneChangeState
 LaneChangeDirection = log.LateralPlan.LaneChangeDirection
 
 LANE_CHANGE_SPEED_MIN = 56 * CV.KPH_TO_MS
-LANE_CHANGE_TIME_MAX = 5.
+LANE_CHANGE_TIME_MAX = 10.
 
 DESIRES = {
   LaneChangeDirection.none: {
