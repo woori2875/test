d238bf6a3f04fa4de2dc4693d88f154da23430b5
hermes0578 <91257081+hermes0578@users.noreply.github.com>
2021-10-20 12:11:11 +0900

    Update thermald.py

--- selfdrive/thermald/thermald.py
+++ selfdrive/thermald/thermald.py
@@ -62,7 +62,6 @@ def read_tz(x):
   except FileNotFoundError:
     return 0
 
-prebuiltfile = '/data/openpilot/prebuilt'
 
 def read_thermal(thermal_config):
   dat = messaging.new_message('deviceState')
@@ -425,13 +424,7 @@ def thermald_thread():
 
       started_ts = None
       if off_ts is None:
-        off_ts = sec_since_boot()
-        
-    prebuiltfile = params.get_bool("PutPrebuilt")
-    if not os.path.isfile(prebuiltfile) and prebuiltlet:
-      os.system("cd /data/openpilot; touch prebuilt")
-    elif os.path.isfile(prebuiltfile) and not prebuiltlet:
-      os.system("cd /data/openpilot; rm -f prebuilt")    
+        off_ts = sec_since_boot()   
 
     # Offroad power monitoring
     power_monitor.calculate(peripheralState, startup_conditions["ignition"])
