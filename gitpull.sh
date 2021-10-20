155d7fb448834d2eb88196cbb3d768d0ba391f8a
hermes0578 <91257081+hermes0578@users.noreply.github.com>
2021-10-20 10:52:47 +0900

    Update gitpull.sh

--- gitpull.sh
+++ gitpull.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/bash
 
 cd /data/openpilot
-echo -n "0" > /data/params/d/PutPrebuilt
-rm -f prebuilt
+
+if [ -f /EON ]; then
+  echo -n "0" > /data/params/d/PutPrebuilt
+  rm -f prebuilt
+elif [ -f /TICI ]; then
+  echo -n "0" > /data/params/d/PutPrebuilt
+  sudo rm -f prebuilt
+fi
+
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 git fetch --all
 REMOTE_HASH=$(git rev-parse --short --verify origin/$BRANCH)
-git reset --hard $REMOTE_HASH 
+git reset --hard $REMOTE_HASH
