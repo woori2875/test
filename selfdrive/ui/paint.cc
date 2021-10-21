59db7af505eb4033714e040e950f15f69146fe3b
hermes0578 <91257081+hermes0578@users.noreply.github.com>
2021-10-21 21:59:24 +0900

    Update paint.cc

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -15,11 +15,13 @@
 #define NANOVG_GLES3_IMPLEMENTATION
 #include <nanovg_gl.h>
 #include <nanovg_gl_utils.h>
-
+#include <time.h> 
+#include <string>
 #include "selfdrive/common/util.h"
 #include "selfdrive/hardware/hw.h"
 #include "selfdrive/ui/ui.h"
 #include "selfdrive/ui/extras.h"
+
 #include "selfdrive/ui/dashcam.h"
 
 #ifdef QCOM2
@@ -1252,6 +1254,56 @@ static void ui_draw_vision_car(UIState *s) { //image designd by" Park byeoung kw
   }
 //bsd
 
+// draw date/time
+void draw_kr_date_time(UIState *s) {
+  int rect_w = 600;
+  const int rect_h = 50;
+  int rect_x = s->fb_w/2 - rect_w/2;
+  const int rect_y = 0;
+  char dayofweek[50];
+
+  // Get local time to display
+  time_t t = time(NULL);
+  struct tm tm = *localtime(&t);
+  char now[50];
+  if (tm.tm_wday == 0) {
+    strcpy(dayofweek, "SUN");
+  } else if (tm.tm_wday == 1) {
+    strcpy(dayofweek, "MON");
+  } else if (tm.tm_wday == 2) {
+    strcpy(dayofweek, "TUE");
+  } else if (tm.tm_wday == 3) {
+    strcpy(dayofweek, "WED");
+  } else if (tm.tm_wday == 4) {
+    strcpy(dayofweek, "THU");
+  } else if (tm.tm_wday == 5) {
+    strcpy(dayofweek, "FRI");
+  } else if (tm.tm_wday == 6) {
+    strcpy(dayofweek, "SAT");
+  }
+
+  if (s->scene.kr_date_show && s->scene.kr_time_show) {
+    snprintf(now,sizeof(now),"%04d-%02d-%02d %s %02d:%02d:%02d", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, dayofweek, tm.tm_hour, tm.tm_min, tm.tm_sec);
+  } else if (s->scene.kr_date_show) {
+    snprintf(now,sizeof(now),"%04d-%02d-%02d %s", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, dayofweek);
+  } else if (s->scene.kr_time_show) {
+    snprintf(now,sizeof(now),"%02d:%02d:%02d", tm.tm_hour, tm.tm_min, tm.tm_sec);
+  }
+
+  nvgTextAlign(s->vg, NVG_ALIGN_CENTER | NVG_ALIGN_TOP);
+  nvgBeginPath(s->vg);
+  nvgRoundedRect(s->vg, rect_x, rect_y, rect_w, rect_h, 0);
+  nvgFillColor(s->vg, nvgRGBA(0, 0, 0, 0));
+  nvgFill(s->vg);
+  nvgStrokeColor(s->vg, nvgRGBA(255,255,255,0));
+  nvgStrokeWidth(s->vg, 0);
+  nvgStroke(s->vg);
+
+  nvgFontSize(s->vg, 50);
+  nvgFillColor(s->vg, nvgRGBA(255, 255, 255, 200));
+  nvgText(s->vg, s->fb_w/2, rect_y, now, NULL);
+}
+
 //tpms
 static void ui_draw_tpms(UIState *s) {
   int viz_tpms_w = 140;
@@ -1330,6 +1382,9 @@ static void ui_draw_vision(UIState *s) {
   // Draw augmented elements
   if (scene->world_objects_visible) {
     ui_draw_world(s);
+  }
+  if ((scene->kr_date_show || scene->kr_time_show) {
+    draw_kr_date_time(s);
   }
   // Set Speed, Current Speed, Status/Events
   ui_draw_vision_header(s);
