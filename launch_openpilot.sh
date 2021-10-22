02018a8ec2d6c0ada8784acd55a52b8d2f5c1346
hermes0578 <91257081+hermes0578@users.noreply.github.com>
2021-10-23 08:38:21 +0900

    Update launch_openpilot.sh

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,27 +1,27 @@
 #!/usr/bin/bash
 
-GET_PROP1=$(getprop persist.sys.locale)
-GET_PROP2=$(getprop persist.sys.local)
-GET_PROP3=$(getprop persist.sys.timezone)
-
-if [ "$GET_PROP1" != "ko-KR" ]; then
-    setprop persist.sys.locale ko-KR
-fi
-if [ "$GET_PROP2" != "ko-KR" ]; then
-    setprop persist.sys.local ko-KR
-fi
-if [ "$GET_PROP3" != "Asia/Seoul" ]; then
-    setprop persist.sys.timezone Asia/Seoul
-fi
-
 if [ ! -f "/system/fonts/opensans_regular.ttf" ]; then
     sleep 3
     mount -o remount,rw /system
-  	cp -rf /data/openpilot/selfdrive/assets/fonts/opensans* /system/fonts/
-    cp -rf /data/openpilot/selfdrive/assets/fonts/fonts.xml /system/etc/fonts.xml
+
+    cp -rf /data/openpilot/installer/fonts/opensans* /system/fonts/
+    cp -rf /data/openpilot/installer/fonts/fonts.xml /system/etc/fonts.xml
     chmod 644 /system/etc/fonts.xml
-  	chmod 644 /system/fonts/opensans*
+    chmod 644 /system/fonts/opensans*
+
+
     mount -o remount,r /system
+
+    setprop persist.sys.locale ko-KR
+    setprop persist.sys.local ko-KR
+    setprop persist.sys.timezone Asia/Seoul
+
+    echo =================================================================
+    echo Ko-KR NanumGothic font install complete
+    echo Ko-KR locale change complete
+    echo =================================================================
+    echo Reboot Now..!!
+    echo =================================================================
     reboot
 fi
 
