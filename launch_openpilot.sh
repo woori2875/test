c89e2abfdb5ccf15325dbccd2502cc4c293e54e2
marine2875 <79120100+marine2875@users.noreply.github.com>
2021-10-23 13:33:34 +0900

    up

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,19 +1,5 @@
 #!/usr/bin/bash
 
-GET_PROP1=$(getprop persist.sys.locale)
-GET_PROP2=$(getprop persist.sys.local)
-GET_PROP3=$(getprop persist.sys.timezone)
-
-if [ "$GET_PROP1" != "ko-KR" ]; then
-    setprop persist.sys.locale ko-KR
-fi
-if [ "$GET_PROP2" != "ko-KR" ]; then
-    setprop persist.sys.local ko-KR
-fi
-if [ "$GET_PROP3" != "Asia/Seoul" ]; then
-    setprop persist.sys.timezone Asia/Seoul
-fi
-
 export PASSIVE="0"
 exec ./launch_chffrplus.sh
 
