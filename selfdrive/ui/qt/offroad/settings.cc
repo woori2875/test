e624936c8fd52ec9a6f83ba9c4087d69c29308fe
hermes0578 <91257081+hermes0578@users.noreply.github.com>
2021-10-23 15:01:47 +0900

    Update settings.cc

--- selfdrive/ui/qt/offroad/settings.cc
+++ selfdrive/ui/qt/offroad/settings.cc
@@ -318,6 +318,8 @@ QWidget * network_panel(QWidget * parent) {
   // SSH key management
   list->addItem(new SshToggle());
   list->addItem(new SshControl());
+  list->addItem(new KRDateToggle());
+  list->addItem(new KRTimeToggle());
   list->addItem(new LateralControlSelect());
   list->addItem(horizontal_line());
   
