ef5c114c85657106d011b728cf39e4321e03e1ff
hermes0578 <91257081+hermes0578@users.noreply.github.com>
2021-10-27 12:39:25 +0900

    Update ssh_keys.h

--- selfdrive/ui/qt/widgets/ssh_keys.h
+++ selfdrive/ui/qt/widgets/ssh_keys.h
@@ -73,6 +73,30 @@ public:
   }
 };
 
+class PrebuiltToggle : public ToggleControl {
+  Q_OBJECT
+
+public:
+  PrebuiltToggle() : ToggleControl("Prebuilt 파일 생성", "Prebuilt 파일을 생성하며 부팅속도를 단축시킵니다. UI수정을 한 경우 기능을 끄십시오.", "../assets/offroad/icon_shell.png", Params().getBool("PutPrebuiltOn")) {
+    QObject::connect(this, &PrebuiltToggle::toggleFlipped, [=](int state) {
+      char value = state ? '1' : '0';
+      Params().put("PutPrebuiltOn", &value, 1);
+    });
+  }
+};
+
+class GitHash : public AbstractControl {
+  Q_OBJECT
+
+public:
+  GitHash();
+
+private:
+  QLabel local_hash;
+  QLabel remote_hash;
+  Params params;
+};
+
 // openpilot Preview
 class OpenpilotView : public AbstractControl {
   Q_OBJECT
