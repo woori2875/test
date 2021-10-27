15dec80281eb6069895a91e4d2c796f4fd1c166f
hermes0578 <91257081+hermes0578@users.noreply.github.com>
2021-10-27 20:19:58 +0900

    Update settings.cc

--- selfdrive/ui/qt/offroad/settings.cc
+++ selfdrive/ui/qt/offroad/settings.cc
@@ -333,11 +333,11 @@ QWidget * network_panel(QWidget * parent) {
   const char* gitpull_cancel = "sh /data/openpilot/gitpull_cancel.sh ''";
   auto gitpull_cancelBtn = new ButtonControl("Git Pull 취소", "실행");
   QObject::connect(gitpull_cancelBtn, &ButtonControl::clicked, [=]() {
-    if (ConfirmationDialog::confirm("GitPull 이전 상태로 되돌립니다. 진행하시겠습니까?", w)){
+    if (ConfirmationDialog::confirm("GitPull 이전 상태로 되돌립니다. 진행하시겠습니까?", this)) {
       std::system(gitpull_cancel);
     }
   });
-  list->addItem(gitpull_canceltbtn);
+  list->addItem(gitpullcanceltbtn);
   list->addItem(horizontal_line());
   
   list->addItem(new SshToggle());
