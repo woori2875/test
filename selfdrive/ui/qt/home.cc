537e1e6f4d8afe38b2751b7fc958382d2835e7d7
hermes0578 <91257081+hermes0578@users.noreply.github.com>
2021-10-28 07:25:05 +0900

    Update home.cc

--- selfdrive/ui/qt/home.cc
+++ selfdrive/ui/qt/home.cc
@@ -91,7 +91,15 @@ void HomeWindow::mousePressEvent(QMouseEvent* e) {
   // Handle sidebar collapsing
   if (onroad->isVisible() && (!sidebar->isVisible() || e->x() > sidebar->width())) {
     sidebar->setVisible(!sidebar->isVisible() && !onroad->isMapVisible());
+    QUIState::ui_state.sidebar_view = !QUIState::ui_state.sidebar_view; // opkr
   }
+  
+  QUIState::ui_state.scene.setbtn_count = 0; // opkr
+  QUIState::ui_state.scene.homebtn_count = 0;
+  if (QUIState::ui_state.scene.started && QUIState::ui_state.scene.scr.autoScreenOff != -2) {
+    QUIState::ui_state.scene.touched2 = true;
+    QTimer::singleShot(500, []() { QUIState::ui_state.scene.touched2 = false; });
+  } // opkr
 }
 
 // OffroadHome: the offroad home page
@@ -202,6 +210,6 @@ void OffroadHome::refresh() {
   update_notif->setVisible(updateAvailable);
   alert_notif->setVisible(alerts);
   if (alerts) {
-    alert_notif->setText(QString::number(alerts) + " ALERT" + (alerts > 1 ? "S" : ""));
+    alert_notif->setText(QString::number(alerts) + " 경고" + (alerts > 1 ? "S" : ""));
   }
 }
