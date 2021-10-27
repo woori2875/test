101e913c6b9bd5f507c95c5c9ead1308ac14d834
hermes0578 <91257081+hermes0578@users.noreply.github.com>
2021-10-28 08:26:50 +0900

    Update ssh_keys.h

--- selfdrive/ui/qt/widgets/ssh_keys.h
+++ selfdrive/ui/qt/widgets/ssh_keys.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <QPushButton>
-
+#include <QSoundEffect> // opkr
 #include "selfdrive/hardware/hw.h"
 #include "selfdrive/ui/qt/widgets/controls.h"
 
@@ -31,6 +31,48 @@ private:
 
   void refresh();
   void getUserKeys(const QString &username);
+};
+//opkr
+class PrebuiltToggle : public ToggleControl {
+  Q_OBJECT
+
+public:
+  PrebuiltToggle() : ToggleControl("Prebuilt 파일 생성", "Prebuilt 파일을 생성하며 부팅속도를 단축시킵니다. UI수정을 한 경우 기능을 끄십시오.", "../assets/offroad/icon_shell.png", Params().getBool("PutPrebuiltOn")) {
+    QObject::connect(this, &PrebuiltToggle::toggleFlipped, [=](int state) {
+      char value = state ? '1' : '0';
+      Params().put("PutPrebuiltOn", &value, 1);
+    });
+  }
+};
+
+class AutoScreenOff : public AbstractControl {
+  Q_OBJECT
+
+public:
+  AutoScreenOff();
+
+private:
+  QPushButton btnplus;
+  QPushButton btnminus;
+  QLabel label;
+  Params params;
+
+  void refresh();
+};
+
+class BrightnessControl : public AbstractControl {
+  Q_OBJECT
+
+public:
+  BrightnessControl();
+
+private:
+  QPushButton btnplus;
+  QPushButton btnminus;
+  QLabel label;
+  Params params;
+
+  void refresh();
 };
 
 // LateralControlSelect
