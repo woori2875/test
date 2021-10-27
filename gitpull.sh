ba91956597ab60ac07220eda4129b2a46007be7f
hermes0578 <91257081+hermes0578@users.noreply.github.com>
2021-10-27 18:37:25 +0900

    Update gitpull.sh

--- gitpull.sh
+++ gitpull.sh
@@ -1,9 +1,21 @@
 #!/usr/bin/bash
 
+export LD_LIBRARY_PATH=/data/data/com.termux/files/usr/lib
+export HOME=/data/data/com.termux/files/home
+export PATH=/usr/local/bin:/data/data/com.termux/files/usr/bin:/data/data/com.termux/files/usr/sbin:/data/data/com.termux/files/usr/bin/applets:/bin:/sbin:/vendor/bin:/system/sbin:/system/bin:/system/xbin:/data/data/com.termux/files/usr/bin/python
+export PYTHONPATH=/data/openpilot
+
 cd /data/openpilot
-echo -n "0" > /data/params/d/PutPrebuilt
-rm -f prebuilt
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
-git fetch --all
-REMOTE_HASH=$(git rev-parse --short --verify origin/$BRANCH)
-git reset --hard $REMOTE_HASH 
+HASH=$(git rev-parse HEAD)
+git fetch
+REMOTE_HASH=$(git rev-parse --verify origin/$BRANCH)
+git pull
+
+if [ "$HASH" != "$REMOTE_HASH" ]; then
+  if [ -f "/data/openpilot/prebuilt" ]; then
+    pkill -f thermald
+    rm -f /data/openpilot/prebuilt
+  fi
+  sleep 5s
+fi
