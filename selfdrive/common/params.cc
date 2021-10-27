0d1235a2911fe7d069b74d0120f2ac165e45e1da
hermes0578 <91257081+hermes0578@users.noreply.github.com>
2021-10-27 11:33:15 +0900

    Update params.cc

--- selfdrive/common/params.cc
+++ selfdrive/common/params.cc
@@ -229,6 +229,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"DisableOpFcw", PERSISTENT},
     {"ShowDebugUI", PERSISTENT},
     {"CustomLeadMark", PERSISTENT},
+    {"PutPrebuiltOn", PERSISTENT},
     {"NDACamera", PERSISTENT},
     {"ShowCgearUI", PERSISTENT},
     {"KRDateShow", PERSISTENT},
